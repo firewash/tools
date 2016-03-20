ca460b9703d8e11fb2938e883d352018b8db17ee
firewash <firewash@gmail.com>
2016-03-20 23:43:24 +0800

    Just work correctly.

--- site-capture/index.js
+++ site-capture/index.js
@@ -4,7 +4,6 @@
  * TODO: 抓图，分析，报警，应该独立成三个不同的任务进程，其中心围绕task表
  *
  */
-
 var phantom = require('phantom');
 //var comparer = require("lib/image-comparer");
 var fs = require("fs");
@@ -20,20 +19,20 @@ function Comparer() {
         errorType: 'movement',
         transparency: 0.3
     });
-
 }
 
 Comparer.prototype = {
     //对比两个文件，并将对比结果图片写入磁盘
     //opt={file1 file2 resultfile ratio}
     diff: function (opt, callback) {
-        console.log("in diff function");
+        console.log("Inner diff function. opt is:",opt);
         var target = opt.target, other = opt.other, resultfile = opt.resultfile, ratio = opt.ratio || 0.01;
 
-        var diff = resemble(target).compareTo(other).ignoreColors().ignoreAntialiasing().onComplete(function (data) {
+        var _diff = resemble(target).compareTo(other).ignoreColors().ignoreAntialiasing().onComplete(function (data) {
             if (+data.misMatchPercentage <= ratio) {
                 callback({msg: "misMatchPercentage is too low,donnot save compare result~"}, null);
             } else {
+                console.log("Save diff image to ",resultfile);
                 data.getDiffImage().pack().pipe(fs.createWriteStream(resultfile));
                 callback(null, opt);
             }
@@ -41,17 +40,14 @@ Comparer.prototype = {
     }
 };
 
-function Capturer() {
-
-}
-
+function Capturer() {}
 Capturer.prototype = {
     /*
      * option={url interval,name_prefix }
      *
      * */
     capture: function (option, callback) {
-        console.log("capture callback : ", typeof callback)
+        console.log("Capture callback : ", typeof callback)
         var url = option.url, interval = option.interval || 1000, name_prefix = option.name_prefix || "tool_site_capture_unknow_site";
 
         var now = new Date();
@@ -74,29 +70,23 @@ Capturer.prototype = {
             _page=page;
             return page.open(url)
         }).then(function (status) {
-            console.log("page open status: " + status);
+            console.log("Page open status: " + status);
             if (status === "success") {
                 var prop = {
                     format: format,
-                    quality: option.quality || '100'
+                    quality: option.quality || '60'
                 };
-                _page.render(folder + filename + "." + format, prop).then(function(result){
-
-                });
-                console.log("page render and save, ok",folder + filename + "." + format);
-                // promise.resolve(option);
-            } else {
-                err = {
-                    status: status,
-                    msg: "page open failed"
-                };
-                //promise.reject(err);
+                return _page.render(folder + filename + "." + format, prop);
             }
+        },function(){
+
+        }).then(function(result){
+            console.log("Page render and save, result: ", result);
             _page.close();
             _ph.exit();
-            console.log("capture callback type is ", typeof callback);
+            console.log("Capture callback type is ", typeof callback);
             callback && callback(err, option);
-        });;
+        });
     }
 };
 
@@ -111,15 +101,12 @@ function DBOperator() {
     this.db = null;
     this.MongoClient = MongoClient;
     this.url = url;
-
     //this.connect();
-
 }
 
 DBOperator.prototype = {
     connect: function (callback) {
         this.MongoClient.connect(this.url, function (err, db) {
-
             console.log(err ? "MongoDB connnect error!" : "MongoDB connnect success~.");
             this.db = db;
             callback && callback(err, db);
@@ -135,10 +122,9 @@ DBOperator.prototype = {
                 callback && callback(err);
                 return;
             }
-            console.log("ready to save db, saveCaptureData:", data);
-            console.log(this.db);
+            console.log("Will save capture data:", data);
             this.db.collection('origin_captures').insertOne(data, function (err, result) {
-                console.log("Inserted Success - saveCaptureData.");
+                console.log("Insert success , in fn saveCaptureData.");
                 callback && callback(err, result);
                 self.close();
             });
@@ -150,7 +136,7 @@ DBOperator.prototype = {
 
             cursor.each(function (err, item) {
                 if (item) {
-                    console.dir(item);
+                    console.log(item);
                 } else {
                     console.log("over");
                 }
@@ -159,7 +145,7 @@ DBOperator.prototype = {
 
     },
     getLastestCapture: function (opt, callback) {
-        console.log("DB getLastestCapture:", opt.url);
+        console.log("Get lastest capture in DB:", opt.url);
         var queryCondition = {
             url: opt.url
         };
@@ -170,17 +156,19 @@ DBOperator.prototype = {
                 return;
             }
             var cursor = this.db.collection("origin_captures").find(queryCondition).sort({key: -1}).limit(1);
-            console.log("query end.");
-            cursor.each(function (err, item) {
-                if (item) {
-                    console.log("find one");
-                    callback(null, item);
-                } else {
-                    callback({msg: "no result."});
-                    console.log("over");
-                }
+            var item = null;
+            console.log("Query end.");
+            if(cursor.hasNext())item = cursor.next();
+            if (item) {
+                console.log("Find one item:",item);
+                item.then(function(data){
+                    callback(null, data);
+                })
                 return;
-            });
+            } else {
+                callback({msg: "No result."});
+            }
+
         });
     }
 };
@@ -215,34 +203,37 @@ function start() {
              * dboperator.getCaptureData();
              * return;
              */
-            console.log("in afterCpture,target is:", target_data);
+            console.log("In afterCpture,target is:", target_data);
             if (err) {
-                console.log("error:", err);
+                console.log("Error:", err);
                 return;
             }
             dboperator.getLastestCapture({url: target_data.url}, function (err, last_data) {
                 if (err || !last_data) {
-                    console.log("error:", err.msg);
+                    console.log("Error:", err.msg);
                     dboperator.saveCaptureData(target_data, function () {
                     });
                     return;
                 }
 
-                console.log("存在历史截图，则对比");
+                console.log("Has a pre capture, now diff with it.");
                 var opt = {
                     target: target_data.folder + target_data.filename + "." + target_data.format,
                     other: last_data.folder + last_data.filename + "." + last_data.format,
-                    resultFile: target_data.folder + target_data.filename + "_diff" + "." + target_data.format
+                    resultfile: target_data.folder + target_data.filename + "_diff" + "." + target_data.format
                 }
                 comparer.diff(opt, function (err, data) {
-                    if (!err) {
-                        target_data.difference = resultFile;
+                    if (err) {
+                        console.log("Diff failed. As,", err.msg);
+                    }else{
+                        console.log("Diff success. add diff info  to target data");
+                        target_data.diffimg = opt.resultfile;
                         target_data.diffwith = last_data._id;
                     }
+                    console.log("Will save target data.")
                     dboperator.saveCaptureData(target_data, function () {
                     });
                 })
-
             });
         }
 
@@ -256,7 +247,5 @@ function start() {
             capturer.capture(opt, afterCapture);
         }
     });
-
 };
-
 start();
