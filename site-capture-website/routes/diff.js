7f3526a2f3fb304a93d61d0b9443493cb436c632
firewash <firewash@gmail.com>
2016-03-24 18:10:50 +0800

    补充view和路由

---
+++ site-capture-website/routes/diff.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var dbreader = require("../lib/dbreader");
+
+function realPath(filename,format){
+    return "/capture/" + filename +"."+ format;
+}
+
+/* GET list page. */
+router.get('/list', function(req, res, next) {
+    res.render('diff/list', { title: '列表' });
+});
+/* GET detail page.
+*  /detail?id=***** 只根据id查询.想查询更多,请使用list的api
+* */
+router.get('/detail', function(req, res, next) {
+    var query = req.query;
+
+    //需要安全的处理一下传入的参数,不要直接传递
+    var opt = {};
+    if(query._id)opt=query._id;
+
+    dbreader.getLastestCapture(opt, function(err, data){
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log("get data callback", data);
+
+        res.render('diff/detail', {
+            id:data.origin_info._id,
+            title:data.title,
+            time:data.time,
+            diffratio:data.origin_info.diffratio,
+            diffwith_img: realPath(data.diffwith_info.filename, data.diffwith_info.format),
+            origin_img: realPath(data.origin_info.filename, data.origin_info.format),
+            diff_img: realPath(data.origin_info.diffimg, data.origin_info.format)
+        });
+        return;
+    });
+});
+
+function getListFromDB(condition, callback){
+    dbreader.getLastestCapture()
+}
+
+module.exports = router;
