593892b5beac1b1194acaa80b957118edc50e437
le.wang <le.wang@shenma-inc.com>
2016-11-23 18:22:47 +0800

    update

--- 导航竞品分析-扩展/code/lib.js
+++ 导航竞品分析-扩展/code/lib.js
@@ -7,22 +7,27 @@ function charFilter(str) {
 }
 
 var tableToExcel = (function() {
-    var uri = 'data:application/vnd.ms-excel;base64,',
-        template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',
-        base64 = function(s) {
-            return window.btoa(unescape(encodeURIComponent(s)))
-        },
-        format = function(s, c) {
-            return s.replace(/{(\w+)}/g,
-                function(m, p) {
-                    return c[p];
-                })
-        }
+    // var uri = 'data:application/vnd.ms-excel;base64,',
+    //     template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',
+    //     base64 = function(s) {
+    //         return window.btoa(unescape(encodeURIComponent(s)))
+    //     },
+    //     format = function(s, c) {
+    //         return s.replace(/{(\w+)}/g,
+    //             function(m, p) {
+    //                 return c[p];
+    //             })
+    //     }
+    var base64 = function(s) {
+        return window.btoa(unescape(encodeURIComponent(s)))
+    }
     return function(table, filename = '下载', name) {
         if (!table.nodeType) table = document.getElementById(table)
-        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML };
-        var content = uri + base64(format(template, ctx));
-        //return window.location.href = content;
+            //var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML };
+            //var content = uri + base64(format(template, ctx));
+            //return window.location.href = content;
+        var content = `<html><head><meta charset="UTF-8"></head><body><table>${table.innerHTML}</table></body></html>`;
+        content = `data:application/vnd.ms-excel;base64,${base64(content)}`;
         var a = document.createElement('a');
         a.download = `${filename}.xls`;
         a.href = content;
