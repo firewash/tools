a6bc5971be047b391ec7f1c624e91f6ef157b7cc
le.wang <le.wang@shenma-inc.com>
2016-11-29 11:11:58 +0800

    页面有点卡，用worker处理结果字符串。

--- 导航竞品分析-扩展/code/background.js
+++ 导航竞品分析-扩展/code/background.js
@@ -133,86 +133,11 @@ function renderResult(sitesArr) {
         let filename = `diff_${hosts.join('_')}_${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
         tableToExcel('resultTable', filename);
     }
-
-    let equalHTML = [];
-    let diffCountHTML = [];
-    let diffLabelHTML = [];
-    let otherHTML = [];
-    let headFragments = [];
-    let htmlFragments = [];
-    const subscript = {
-        "equal": 0,
-        "diff-count": 1,
-        "diff-label": 2,
-        "new": 3
-    }
-    sitesArr.forEach(function(site, siteIndex) {
-        let tabInfo = site.tab;
-        let siteData = site.data;
-        if (headFragments.length === 0) {
-            headFragments.push('<tr>');
-        }
-        headFragments.push(`<td colspan=6>${tabInfo.title}</td>`)
-        if (siteIndex === sitesArr.length - 1) {
-            headFragments.push('</tr>');
-        }
-        let tempSiteHTML = [
-            [],
-            [],
-            [],
-            []
-        ];
-        let dataKeys = Object.keys(siteData);
-        dataKeys.forEach(function(dataKey, dataIndex) {
-            let url = dataKey;
-            let link = siteData[dataKey];
-            let cflag = link.cflag;
-            let items = link.items;
-            let labels = Object.keys(items).join(";");
-            let count = labels.length;
-            let str = ` <td class="${link.cflag}">${link.cflag}</td>
-                    <td class="${link.cflag}" title='出现${count}次'>${count>1?count:''}</td>
-                    <td class="${link.cflag}">${labels}</td>
-                    <td class="${link.cflag}">${url}</td>
-                    <td class="${link.cflag}"><button data-tabid="${tabInfo.id}" data-url="${url}" title="在tab页中查看"></button></td>`; //<td class='counter'></td>
-            tempSiteHTML[subscript[cflag]].push(str);
-
-        });
-        var cc = [];
-        tempSiteHTML.forEach(function(set) {
-            cc = cc.concat(set);
-        });
-        htmlFragments.push(cc);
+    
+    worker.postAndListen('calResultTableHTML', sitesArr, function(data){
+        $("#result").innerHTML = data;
     });
 
-    let maxLength = 0;
-    let siteCount = htmlFragments.length;
-    htmlFragments.forEach(function(set) {
-        maxLength = Math.max(maxLength, set.length);
-    });
-    let concatStr = '';
-    for (var i = 0; i < maxLength; i++) {
-        concatStr += "<tr>"
-        for (var j = 0; j < siteCount; j++) {
-            concatStr += `<td class="counter">${i+1}</td>${htmlFragments[j][i]||"<td></td><td></td><td></td><td></td><td></td>"}`;
-        }
-        concatStr += "</tr>"
-    }
-
-    let html = `<table id="resultTable">
-                <thead>
-                    ${ headFragments.join('')}
-                    <tr style="background:#BDD7EE;text-align:center;font-weight:bold;">
-                        <td style='width:20px;'>序号</td><td style='width:40px;'>标识</td><td style='width:20px;'>出现次数</td><td style='width:200px;'>标题</td><td style='width:200px;'>URL</td><td style='width:60px;'>操作</td>
-                        <td style='width:20px;'>序号</td><td style='width:40px;'>标识</td><td style='width:20px;'>出现次数</td><td style='width:200px;'>标题</td><td style='width:200px;'>URL</td><td style='width:60px;'>操作</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${concatStr}
-                </tbody>
-                </table>`;
-
-    $("#result").innerHTML = html;
     return true;
 }
 
@@ -277,27 +202,27 @@ function visitDOMInTab(selector, tabid) {
     var animCSS = "@-webkit-keyframes jingpinfenxi_focus_anim {" + "0%{box-shadow: inset 0px 0px 15px 5px red, 0px 0px 15px 5px red;}" + "100%{box-shadow: 0px 0px 0px 0px red, 0px 0px 0px 0px red;}" + "}" + ".jingpinfenxi_focus{-webkit-animation: jingpinfenxi_focus_anim 1s infinite linear;}";
     chrome.tabs.executeScript(+tabid, {
         code: ` (function() {
-                                debugger;
-                                if (!document.getElementById('style_style_jingpinfenxi_focus')) {
-                                    var style = document.createElement("style");
-                                    style.id = 'style_style_jingpinfenxi_focus';
-                                    style.innerHTML = "${animCSS}";
-                                    document.head.appendChild(style);
-                                }
+                    debugger;
+                    if (!document.getElementById('style_style_jingpinfenxi_focus')) {
+                        var style = document.createElement("style");
+                        style.id = 'style_style_jingpinfenxi_focus';
+                        style.innerHTML = "${animCSS}";
+                        document.head.appendChild(style);
+                    }
 
-                                if (window.lastShowedLinks) {
-                                    for (let i = 0, len = lastShowedLinks.length; i < len; i++) {
-                                        lastShowedLinks[i].classList.remove("jingpinfenxi_focus");
-                                    }
-                                }
-                                let doms = window.lastShowedLinks = document.querySelectorAll("${selector}");
-                                for (let i = 0, len = doms.length; i < len; i++) {
-                                    doms[i].classList.add("jingpinfenxi_focus");
-                                }
-                                doms[0] && doms[0].scrollIntoView();
-                                document.body.scrollTop -= 200;
-                            })();
-                            `
+                    if (window.lastShowedLinks) {
+                        for (let i = 0, len = lastShowedLinks.length; i < len; i++) {
+                            lastShowedLinks[i].classList.remove("jingpinfenxi_focus");
+                        }
+                    }
+                    let doms = window.lastShowedLinks = document.querySelectorAll("${selector}");
+                    for (let i = 0, len = doms.length; i < len; i++) {
+                        doms[i].classList.add("jingpinfenxi_focus");
+                    }
+                    doms[0] && doms[0].scrollIntoView();
+                    document.body.scrollTop -= 200;
+                })();
+                `
     }, function(data) {});
 
 }
@@ -325,6 +250,34 @@ var progress = (function() {
             dom.style.display = "none";
         }
     }
+})();
+
+//字符串计算等繁重任务
+var worker = (function(){
+    var w = new Worker("worker.js");
+    w.addEventListener('message', function(event) {
+        console.log('worker message: ', event.data.type, event.data.value);
+        listeners[event.data.type](event.data.value);
+    }, false);
+    var listeners = {};
+    var addOnceTypeListener = function(type, fn){
+        var once = function(){
+            fn.apply(window, arguments);
+            delete listeners[type];
+        }
+        listeners[type] = once;
+    }
+    var postAndListen = function(type, data, response){
+        w.postMessage({
+            type: type,
+            value: data
+        });
+        addOnceTypeListener(type, response);
+    };
+    return {
+        postAndListen
+    }
+      
 })();
 
 //初始化
@@ -369,9 +322,7 @@ function init() {
         let section = null;
         if (target.tagName === "BUTTON") {
             let tabid = target.dataset.tabid;
-            let selector = `
-                            a[data - url = '${target.dataset.url}']
-                            `;
+            let selector = `a[data-url = '${target.dataset.url}']`;
             visitDOMInTab(selector, tabid);
         }
     }
---
+++ 导航竞品分析-扩展/code/worker.js
@@ -0,0 +1,96 @@
+
+// 结果列表的html
+function calResultTableHTML(sitesArr){
+    let equalHTML = [];
+    let diffCountHTML = [];
+    let diffLabelHTML = [];
+    let otherHTML = [];
+    let headFragments = [];
+    let htmlFragments = [];
+    const subscript = {
+        "equal": 0,
+        "diff-count": 1,
+        "diff-label": 2,
+        "new": 3
+    }
+    sitesArr.forEach(function(site, siteIndex) {
+        let tabInfo = site.tab;
+        let siteData = site.data;
+        if (headFragments.length === 0) {
+            headFragments.push('<tr>');
+        }
+        headFragments.push(`<td colspan=6>${tabInfo.title}</td>`)
+        if (siteIndex === sitesArr.length - 1) {
+            headFragments.push('</tr>');
+        }
+        let tempSiteHTML = [
+            [],
+            [],
+            [],
+            []
+        ];
+        let dataKeys = Object.keys(siteData);
+        dataKeys.forEach(function(dataKey, dataIndex) {
+            let url = dataKey;
+            let link = siteData[dataKey];
+            let cflag = link.cflag;
+            let items = link.items;
+            let labels = Object.keys(items).join(";");
+            let count = labels.length;
+            let str = ` <td class="${link.cflag}">${link.cflag}</td>
+                    <td class="${link.cflag}" title='出现${count}次'>${count>1?count:''}</td>
+                    <td class="${link.cflag}">${labels}</td>
+                    <td class="${link.cflag}">${url}</td>
+                    <td class="${link.cflag}"><button data-tabid="${tabInfo.id}" data-url="${url}" title="在tab页中查看"></button></td>`; //<td class='counter'></td>
+            tempSiteHTML[subscript[cflag]].push(str);
+
+        });
+        var cc = [];
+        tempSiteHTML.forEach(function(set) {
+            cc = cc.concat(set);
+        });
+        htmlFragments.push(cc);
+    });
+
+    let maxLength = 0;
+    let siteCount = htmlFragments.length;
+    htmlFragments.forEach(function(set) {
+        maxLength = Math.max(maxLength, set.length);
+    });
+    let concatStr = '';
+    for (var i = 0; i < maxLength; i++) {
+        concatStr += "<tr>"
+        for (var j = 0; j < siteCount; j++) {
+            concatStr += `<td class="counter">${i+1}</td>${htmlFragments[j][i]||"<td></td><td></td><td></td><td></td><td></td>"}`;
+        }
+        concatStr += "</tr>"
+    }
+
+    let html = `<table id="resultTable">
+                <thead>
+                    ${ headFragments.join('')}
+                    <tr style="background:#BDD7EE;text-align:center;font-weight:bold;">
+                        <td style='width:20px;'>序号</td><td style='width:40px;'>标识</td><td style='width:20px;'>出现次数</td><td style='width:200px;'>标题</td><td style='width:200px;'>URL</td><td style='width:60px;'>操作</td>
+                        <td style='width:20px;'>序号</td><td style='width:40px;'>标识</td><td style='width:20px;'>出现次数</td><td style='width:200px;'>标题</td><td style='width:200px;'>URL</td><td style='width:60px;'>操作</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${concatStr}
+                </tbody>
+                </table>`;
+     return html;           
+}
+
+
+
+onmessage =function(event) {
+    var data = event.data;
+    switch(data.type) {
+        case 'calResultTableHTML':
+            postMessage({
+                type: 'calResultTableHTML',
+                value: calResultTableHTML(data.value)
+            });
+            break;
+    }
+};
